355880d85ba57f0fb51d176aa906ec0efcb8fe19
spcodegen <shehansp2000@gmail.com>
2024-08-14 11:33:55 +0530

    Save data to Local Storage with Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_sphere/models/note_model.dart';
+import 'package:notes_sphere/models/todo_model.dart';
 import 'package:notes_sphere/utils/router.dart';
 import 'package:notes_sphere/utils/theme_data.dart';
 
-void main() {
+void main() async {
+  //initialize hive
+  await Hive.initFlutter();
+
+  //register the adapters
+  Hive.registerAdapter(NoteAdapter());
+  Hive.registerAdapter(TodoAdapter());
+
+  // open hive boxes
+  await Hive.openBox('notes');
+  await Hive.openBox('todos');
+
   runApp(const MyApp());
 }
 
---
+++ lib/models/todo_model.dart
@@ -0,0 +1,30 @@
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'todo_model.g.dart';
+
+@HiveType(typeId: 1)
+class Todo {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String title;
+
+  @HiveField(2)
+  final DateTime date;
+
+  @HiveField(3)
+  final DateTime time;
+
+  @HiveField(4)
+  final bool isDone;
+
+  Todo({
+    String? id,
+    required this.title,
+    required this.date,
+    required this.time,
+    required this.isDone,
+  }) : id = id ?? const Uuid().v4();
+}
